5dd252026df3449d9c58d31c31c603740c59efd3
Lisa H. <neuroticNanobot@outlook.com>
2025-08-20 19:40:02 -0400

    there was an attempt

--- Templates/JavaScript/src/kata.js
+++ Templates/JavaScript/src/kata.js
@@ -1,7 +1,27 @@
 class Kata {
-    hello(name) {
-        return `Hello ${name}`;
+  validateDigitIntegrity(packetId) {
+    let current = packetId;
+    let sumDigits = 0;
+
+    while (current >= 1) {
+      sumDigits += current % 10;
+      current = Math.floor(current / 10);
     }
+
+    // let sumDigits = digits.reduce((acc, curr) => acc + curr, initialValue);
+    return packetId % sumDigits === 0;
+
+  }
+
+  bulkPacketFilter(max) {
+    let filter = [];
+    for (let i = 0; i < max; i++) {
+      if (this.validateDigitIntegrity(i)) {
+        filter.push(i)
+      }
+    }
+    return filter;
+  }
 }
 
 module.exports = Kata;
--- Templates/JavaScript/src/kata.spec.js
+++ Templates/JavaScript/src/kata.spec.js
@@ -8,7 +8,56 @@ beforeEach(() => {
     fixture = new Kata();
 });
 
-test('hello should return message with specified name', () => {
-    const name = 'Ingage';
-    expect(fixture.hello(name)).toEqual(`Hello ${name}`);
+test('validateDigitIntegrity should be a function', () => {
+    expect(fixture.validateDigitIntegrity).toBeInstanceOf(Function);
 });
+
+test('validateDigitIntegrity given 1 should return true', () => {
+    const actual = fixture.validateDigitIntegrity(1);
+    expect(actual).toBe(true);
+});
+
+test("validateDigitIntegrity given 18 should return true", () => {
+  const actual = fixture.validateDigitIntegrity(18);
+  expect(actual).toBe(true);
+});
+
+test('validateDigitIntegrity given 19 should return false', () => {
+  const actual = fixture.validateDigitIntegrity(19);
+  expect(actual).toBe(false);
+})
+
+describe('validateDigitIntegrity', () => {
+  it.each([
+    [1, true],
+    [2, true],
+    [3, true],
+    [4, true],
+    [5, true],
+    [11, false],
+    [18, true],
+    [19, false],
+  ])('should correctly identify if "%d" is valid digital integrity', (packetId, expected) => {
+    const actual = fixture.validateDigitIntegrity(packetId)
+    expect(actual).toBe(expected);
+  });
+});
+
+test('bulkPacketFilter should process the 1st million valid packet IDs in under 1 sec', () => {
+    const start = Date.now();
+    const actual = fixture.bulkPacketFilter(1_000_000);
+    const end = Date.now();
+    expect(actual).toBeDefined();
+    expect(end - start).toBeLessThan(1000);
+})
+
+test('bulkPacketFilter given 20 should produce 12 valid packet IDs', () => {
+    const actual = fixture.bulkPacketFilter(20);
+    expect(actual.length).toBe(12);
+    expect(actual).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 18]);
+})
+
+test('bulkPacketFilter given 1000 should produce 212 valid packet IDs', () => {
+    const actual = fixture.bulkPacketFilter(1_000);
+    expect(actual.length).toBe(212);
+})
